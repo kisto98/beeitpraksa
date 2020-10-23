1eebfdc309c05329720f2cef29a956cdfee59cc0
kisto98 <stefanbulovic@gmail.com>
2020-10-23 22:27:19 +0200

    DependencyInjection
    Dodao injection i izlozio ime opis i sliku

--- modules/custom/movie_controller/src/Controller/MovieController.php
+++ modules/custom/movie_controller/src/Controller/MovieController.php
@@ -3,30 +3,50 @@
 namespace Drupal\movie_controller\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Database\Database;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use \Drupal\Core\Entity\EntityTypeManagerInterface;
 
-class MovieController {
+class MovieController extends ControllerBase {
 
 
-  
-public function movie() {
-      
-  
-  $db = Database::getConnection();
-    $query = $db->select('node__field_movie_title', 'nrft');
-    $query->join('node__field_description', 'nrfd', 'nrfd.entity_id = nrft.entity_id');
-    $query->join('node__field_imagename', 'nrfp', 'nrfd.entity_id = nrfp.entity_id');
-    $query->fields('nrft', ['field_movie_title_value']);
-    $query->fields('nrfd', ['field_description_value']);
-    $query->fields('nrfp', ['field_imagename_alt']);
-    $result = $query->execute()->fetchAll();
-  
-    return [
-      '#theme' => 'movie',
-      '#datas' => $result,
-      '#title' => 'movie list'
-    ];
-   
- }
+public $entityQuery;
+public $entityTypeManager;
+
+   public function __construct($entityQuery, EntityTypeManagerInterface $entityTypeManager)
+{
+  $this->entityQuery=$entityQuery;
+ $this->entityTypeManager=$entityTypeManager;
+
 }
+public static function create(ContainerInterface $container){
+return new static (
+  $container-> get('entity.query'),
+  $container-> get('entity_type.manager')
 
+);
+}
+ 
+ 
+ public function movie(){
+ 
+   $nids=$this->entityQuery->get('node')->condition('type', 'movie')->execute();
+   $node_storage = $this->entityTypeManager()->getStorage('node');  
+   $nodes = $node_storage->loadMultiple($nids);
+     
+      foreach($nodes as $node) {
+      $items[] = array(
+      'filmime'  => $node->field_movie_title->value,
+      'opis' => $node->field_description->value,
+      'slika'=> $node->field_imagename->entity->getFileUri()    
+    
+    );
+        
+  }
+  
+    return array(
+      '#datas'=> $items, 
+      '#title'=> 'Lista filmova',
+      '#theme'=>'movie'
+    );
+  }
+}
